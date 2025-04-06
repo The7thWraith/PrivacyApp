3136e3d7fae6600e4fe9f341de88db13ef3f6c44
Beatzoid <43425381+Beatzoid@users.noreply.github.com>
2025-04-05 22:17:52 -0700

    restructure

---
+++ frontend/src/pages/Welcome.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { useNavigate } from "react-router-dom";
+
+const Welcome = () => {
+	const navigate = useNavigate();
+
+	return (
+		<div>
+			<p>Welcome</p>
+
+			
+		</div>
+	);
+};
+
+export default Welcome;
